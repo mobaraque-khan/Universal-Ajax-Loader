d42ef5f54ff333327c327aa72c013497e4c8e482
Mobaraque-khan <mk2956806@gmail.com>
2025-10-20 18:14:40 -0700

    last update

--- includes/class-ual-loader.php
+++ includes/class-ual-loader.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:includes/class-ual-loader.php
 <?php
 if (!defined('ABSPATH')) exit;
 
@@ -38,44 +37,3 @@ class UAL_Loader {
     }
 
 }
-=======
-<?php
-if (!defined('ABSPATH')) exit;
-
-class UAL_Loader {
-    public function init() {
-        // Admin area
-        if (is_admin()) {
-            require_once plugin_dir_path(__FILE__) . 'admin/class-ual-admin.php';
-            new UAL_Admin();
-        }
-
-        //css file
-        if (is_admin()) {
-            add_action('admin_enqueue_scripts', [$this, 'enqueue_admin_assets']);
-        }
-
-
-        // Public
-        require_once plugin_dir_path(__FILE__) . 'public/class-ual-ajax.php';
-        require_once plugin_dir_path(__FILE__) . 'public/class-ual-shortcode.php';
-
-        // Enqueue
-        add_action('wp_enqueue_scripts', [$this, 'enqueue_assets']);
-    }
-
-    public function enqueue_assets() {
-        wp_enqueue_style('ual-frontend-style', plugin_dir_url(__FILE__) . '../assets/css/frontend-style.css');
-        wp_enqueue_script('ual-script', plugin_dir_url(__FILE__) . '../assets/js/ajax-loader.js', ['jquery'], null, true);
-        wp_localize_script('ual-script', 'ual_ajax', [
-            'ajax_url' => admin_url('admin-ajax.php')
-        ]);
-    }
-
-    // Enqueue admin CSS
-    public function enqueue_admin_assets() {
-        wp_enqueue_style('ual-admin-style', plugin_dir_url(__FILE__) . '../assets/css/admin-style.css');
-    }
-
-}
->>>>>>> 5bb0325c8cfdf379d5eb821070aaf188584b898c:universal-ajax-loader/includes/class-ual-loader.php
--- universal-ajax-loader/README.md
+++
@@ -1,2 +0,0 @@
-# Universal-Ajax-Loader
- Load posts/products via Ajax with customizable settings.
